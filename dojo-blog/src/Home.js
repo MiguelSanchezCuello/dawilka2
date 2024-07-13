12523a8e8de689e345641f195b634aefa62e93d4
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-13 17:55:15 -0400

    Video #8 shows a way to use the onClick event and how to get access to the event and its properties.
    for the handleclick function we can see that the function gets access automatically to the event so if we want we can add it as a function parameter and then use it.
    
    For the handleClickAgain function it doesnt work quite the same because we are using an anonymous function to call the function we want to send with a parameter so we need to send the event "e" within the anonymous parenthesis (e) => {useEvent(e)} then we can send the event "e" to the parameters and use it. within the definition of our function.

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("clicked", e.target);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("clicked again", name, e.target);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("mario", e)}>
+        Click me Again
+      </button>
     </div>
   );
 };
