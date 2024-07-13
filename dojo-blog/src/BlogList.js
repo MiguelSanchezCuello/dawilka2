0e9a5a4807fadc7ed1ab4c488c8d1b37d7e2b425
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-13 18:55:36 -0400

    Created new component BlogList and learned about props and destructuring the props object and how to pass data from one parent component to a children component.

---
+++ dojo-blog/src/BlogList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BlogList = ({ blogs, title }) => {
+  // These lines are commented because  line above
+  // Instead of using BlogList = (props) => ...
+  // we opted to destucture directly into the props object received...
+  // we are using BlogList = ({blogs, title}) => ... and it works the same as
+  // const blogs = props.blogs;
+  // const title = props.title;
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
