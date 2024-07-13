bf0ff6a173c66b0dd88517da1f497b2d3a51ce40
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-13 18:32:23 -0400

    how to access lists in react, used the useState hook and the map method. also added the styles for the blongs in the index.css file.

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,21 +2,35 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = (e) => {
-    setName("Luigi");
-    setAge(30);
-    console.log(name);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "Yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "Luigi",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-
-      <p>{name} is {age} years old</p>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
