0b9401536f0dc24f8f505ee283ac0b0165f7ba90
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-14 07:59:56 -0400

    Video #15 explains the useEffect dependecies array, it is a second argument to the useEffect() function>>> useEffect(() => { console.log("useEffect ran");    console.log(blogs); console.log(name)   }, [name]) <<< in this argument we tell theuse effect which variable we want to track for changes and useEffect will only run when the variables in this array are changed, this is good for performance so it will not rerender elements that dont need to rerender.

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -28,6 +28,11 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("useEffect - Home");
+    console.log(blogs)
+  })
 
   return (
     <div className="home">
