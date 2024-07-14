e0b93f9d5137a2028bfb0fb1c5d5054bb09c7e7b
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-13 22:48:28 -0400

    In this session we learned how to pass functions into other components as props, we defined the function to delete blogs using the state in the Home component and passed it down to the BlogList as a prop. The delete button is on each one of the blogs in the BlogList but the function is called there but the function defined in the Home will execute in the home and will make changes using the state, this will force the elements to re-render and update the list.

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   // These lines are commented because  line above
   // Instead of using BlogList = (props) => ...
   // we opted to destucture directly into the props object received...
   // we are using BlogList = ({blogs, title}) => ... and it works the same as
   // const blogs = props.blogs;
   // const title = props.title;
+
+
 
   return (
     <div className="blog-list">
@@ -15,6 +17,7 @@ const BlogList = ({ blogs, title }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
