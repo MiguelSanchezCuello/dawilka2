413ec397d9e4b2e2c236d69aa5ee45dbb5398ee6
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-15 21:52:20 -0400

    Completed the delete option into the BlogDetails component.
    added the delete button with an onClick even which calls a handleClick function, this function makes a fetch request to our endpoint and uses the method DELETE, as asynchronous we logged into the console and also rerouted the user back to the home page using the useHistory hook.

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,13 +1,24 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Blog deleted");
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -17,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
